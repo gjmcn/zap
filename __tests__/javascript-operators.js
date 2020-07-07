b4360a3013e024b325d408cad5935c39c6216e24
gmcn <gjmcn@users.noreply.github.com>
2020-07-07 16:55:03 +0200

    update javascript operator tests

--- __tests__/javascript-operators.js
+++ __tests__/javascript-operators.js
@@ -1,46 +1,59 @@
 const {compute, testEach} = require('./test-helpers.js');
 
-testEach('$delete', [
-  ['o = # u 5 v 6; o $delete "u"; o', {v: 6}],
-  ['o = # u 5 v (# w 6 x 7); o "v" $delete "w"; o', {u: 5, v: {x: 7}}],
+testEach('delete', [
+  [`
+o = # u 5 v 6
+o delete "u"
+o`, {v: 6}
+  ]
 ], 'true');
 
-testEach('$in', [
-  ['"u" $in (# u 5 v 6)', true],
-  ['"u" $in (# v 6)',     false],
+testEach('in', [
+  ['"u" in (# u 5 v 6)', true],
+  ['"u" in (# v 6)',     false],
 ]);
 
-testEach('$instanceof', [
-  ['$new Date $instanceof Date',   true],
-  ['$new Date $instanceof RegExp', false],
+testEach('instanceof', [
+  ['new Date instanceof Date',   true],
+  ['new Date instanceof RegExp', false],
 ]);
 
-test('$typeof', () => {
-  expect(compute('$typeof 5')).toBe('number');
+test('typeof', () => {
+  expect(compute('typeof 5')).toBe('number');
 });
 
-test('$yield', () => {
-  expect(compute('\\[** $yield 5; $yield 6] $array')).toStrictEqual([5, 6]);
-});
+test('yield', () => {
+  expect(compute(`
+f = fun
+    yield 5
+    yield 6
+\\f array`)).toStrictEqual([5, 6])});
 
-test('$yieldFrom', () => {
-  expect(compute('\\[** $yieldFrom (@ 5 6)] $array')).toStrictEqual([5, 6]);
-});
+test('yieldFrom', () => {
+  expect(compute(`
+f = fun
+    @ 5 6 yieldFrom
+\\f array`)).toStrictEqual([5, 6])});
 
-testEach('$void', [
-  ['$void 0',          undefined],
-  ['5 $void',          undefined],
-  ['$void (x = 5)',    undefined],
-  ['$void (x = 5); x', 5],
+testEach('void', [
+  ['void 0',        undefined],
+  ['5 void',        undefined],
+  ['void (x = 5)',  undefined],
+  [`
+void (x = 5)
+x`, 5
+  ],
 ]);
 
-testEach('$new', [
-  ['$new Date $instanceof Date',    true],
-  ['$new Date "2050" |getFullYear', 2050],
-  ['Date $new 2050 11 |getMonth',   11],
+testEach('new', [
+  ['new Date instanceof Date',     true],
+  ['new Date "2050" ~getFullYear', 2050],
+  ['Date new 2050 11 ~getMonth',   11],
 ]);
 
-test('$await', async () => {
-  const val = await compute(`x = \\{5} $await; x + 10;`, {asyncIIFE: true});
-  expect(val).toBe(15);
+test('await', async () => {
+  const val = await compute(`
+20 period await  
+5`, {asyncIIFE: true});
+  expect(val).toBe(5);
 });
