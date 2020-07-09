ad1c92aa3afe0d378e1a39ba3e73db01ad4987cc
gmcn <gjmcn@users.noreply.github.com>
2020-07-09 20:24:01 +0200

    remove nav

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,19 +1,19 @@
 (() => {
 
-  // const scrollama = require("scrollama");
+  const scrollama = require("scrollama");
 
-  // const scroller = scrollama();
+  const scroller = scrollama();
 
-  // scroller
-  //   .setup({
-  //     step: "h2"
-  //   })
-  //   .onStepEnter(({ element, index, direction }) => {
-  //     console.log(element.innerHTML);
-  //   })
-  //   .onStepExit(response => {
-  //     // { element, index, direction }
-  //   });
+  scroller
+    .setup({
+      step: "h2"
+    })
+    .onStepEnter(({ element, index, direction }) => {
+      console.log(element.innerHTML);
+    })
+    .onStepExit(response => {
+      // { element, index, direction }
+    });
 
 })();
 
